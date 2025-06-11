e173dac9c02d78d4d691823f6d86c8fad41a65dc
Jeremiah Karanja <jeremiahkaranja584@gmail.com>
2025-06-11 00:21:38 -0700

    Add mandatory Python exercise scripts with solutions

---
+++ finance_calculator.py
@@ -0,0 +1,9 @@
+ 
+income = float(input("Enter your monthly income: "))
+expenses = float(input("Enter your total monthly expenses: "))
+
+monthly_savings = income - expenses
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${annual_savings}.")
